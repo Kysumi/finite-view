b14415a7e088e404e8fa775b58d3662eccd7748f
Maunder <maunderscott@gmail.com>
2024-07-01 23:26:39 +1200

    Adding checkbox column

--- src/renderer/src/components/QueryBuilder.tsx
+++ src/renderer/src/components/QueryBuilder.tsx
@@ -13,7 +13,7 @@ import { Input } from '@renderer/components/ui/input';
 import { Button } from '@renderer/components/ui/button';
 import { TableInfo } from '../../../main/actions/getTableInformation';
 import { QueryPropsSchema, TQueryProps } from '../../../main/validators';
-import { flexRender, getCoreRowModel, HeaderContext, useReactTable } from '@tanstack/react-table';
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import {
   Table,
   TableBody,
@@ -23,6 +23,7 @@ import {
   TableRow
 } from '@renderer/components/ui/table';
 import { DataTableColumnHeader } from '@renderer/components/table/Column';
+import { Checkbox } from '@renderer/components/ui/checkbox';
 
 export const QueryBuilder = ({ tableInfo }: { tableInfo: TableInfo | undefined }) => {
   const query = trpc.table.queryTable.useMutation();
@@ -152,64 +153,122 @@ export const QueryBuilder = ({ tableInfo }: { tableInfo: TableInfo | undefined }
   );
 };
 
-const DataTable = ({ data }: { data: Record<string, any>[] }) => {
+const DataTable = ({
+  data
+}: {
+  data: Record<string, string | number | NonNullable<unknown>>[];
+}) => {
   const keySet = new Set<string>();
 
   data?.forEach((obj) => {
     Object.keys(obj).forEach((key) => keySet.add(key));
   });
 
-  const columns = Array.from(keySet).map((key) => ({
+  const columns: ColumnDef<Record<string, string | number | NonNullable<unknown>>>[] = Array.from(
+    keySet
+  ).map((key) => ({
     accessorKey: key,
-    header: ({ column }: HeaderContext<any, any>) => (
-      <DataTableColumnHeader column={column} title={key} />
-    )
+    header: ({ column }) => <DataTableColumnHeader column={column} title={key} />,
+    cell: ({ row }) => {
+      const value = row.original[key];
+      console.log(row);
+
+      console.log(value);
+
+      if (typeof value === 'string') {
+        return <div>{value}</div>;
+      }
+
+      if (Array.isArray(value)) {
+        return <div>Not supported</div>;
+      }
+
+      if (typeof value === 'object') {
+        return <div>{JSON.stringify(value)}</div>;
+      }
+
+      if (typeof value === 'undefined') {
+        return <div></div>;
+      }
+
+      throw new Error(`Unsupported data type for ${key}`);
+    }
   }));
+
+  columns.unshift({
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false
+  });
 
   const table = useReactTable({
     data: data,
     columns,
-    getCoreRowModel: getCoreRowModel()
+    getCoreRowModel: getCoreRowModel(),
+    columnResizeMode: 'onChange',
+    enableColumnResizing: true
   });
 
   return (
-    <div className="rounded-md border mt-8">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <div className={'mt-4'}>
+      <div className="flex-1 text-sm text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} of{' '}
+        {table.getFilteredRowModel().rows.length} row(s) selected.
+      </div>
+      <div className="rounded-md border ">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
